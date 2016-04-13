c7f0ac667d14c0053f0ad77bf3296687a9e0afad
git <webrtc2016>
2016-04-13 17:59:12 +0800

    repository

--- src/main/java/com/drpeng/pengxin/api/dao/UserDao.java
+++ src/main/java/com/drpeng/pengxin/api/dao/UserDao.java
@@ -1,7 +1,9 @@
 package com.drpeng.pengxin.api.dao;
 
 import com.drpeng.pengxin.api.domain.User;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserDao {
 
 	User getUserById(Long id);
