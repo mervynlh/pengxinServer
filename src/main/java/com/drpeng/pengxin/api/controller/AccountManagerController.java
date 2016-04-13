a58c96fee34101a45e780718b169c0b5c91779b1
git <webrtc2016>
2016-04-13 21:03:45 +0800

    33

--- src/main/java/com/drpeng/pengxin/api/controller/AccountManagerController.java
+++ src/main/java/com/drpeng/pengxin/api/controller/AccountManagerController.java
@@ -27,7 +27,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class AccountManagerController extends BaseController {
 
-	private  String a = "2";
+	private  String a = "3";
 
 	@Autowired
 	private UserService userService;
